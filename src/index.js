dd6332ee9cc29bd20ea4c97eb9b6691e844ccb13
Tatarintsev Alexey <tatarincev.alexey@advcake.com>
2022-04-21 16:34:11 +0300

    lesson 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+// import App from './App';
+import reportWebVitals from "./reportWebVitals"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+const messages = []
+
+const addMessages = () => {
+  messages.push("test1")
+  render()
+}
+
+const Message = ({ message }) => {
+  return (
+    <React.Fragment>
+      <h1>{message}</h1>
+    </React.Fragment>
+  )
+}
+
+const MessageField = ({ messages }) => {
+  return (
+    <>
+      <Button />
+      {messages.map((message, index) => (
+        <Message message={message} key={index} />
+      ))}
+    </>
+  )
+}
+
+const Button = () => {
+  return (
+    <React.Fragment>
+      <button onClick={addMessages}>add</button>
+    </React.Fragment>
+  )
+}
+
+const render = () => {
+  root.render(<MessageField messages={messages} />)
+}
+
+render()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
